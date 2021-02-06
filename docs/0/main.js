b783ac94259dc9ab6b03310d450c12fe435a7895
ytyaru <yttry0@gmail.com>
2021-02-06 09:57:11 +0900

    change

--- docs/0/main.js
+++ docs/0/main.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', (event) => {
-    const msg = 'Hello JavaScript !!'
-    console.log(msg);
-    alert(msg);
+    document.querySelector(`#back-color`).addEventListener('change', (event) => {
+        const colors = Color.getColors(event.target.value);
+        document.querySelector(`#fore-color`).setAttribute('value', colors.fore);
+        document.querySelector(`#selected-back-color`).setAttribute('value', colors.selectedBack);
+        document.querySelector(`#selected-fore-color`).setAttribute('value', colors.selectedFore);
+        document.querySelector(`#shadow-color`).setAttribute('value', colors.shadow);
+    });
 });

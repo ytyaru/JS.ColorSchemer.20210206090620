89d3b86b67557e7eb736f809fe17fe54d081e5aa
ytyaru <yttry0@gmail.com>
2021-02-06 12:37:09 +0900

    query bug fix

--- docs/3/ColorSchemer.js
+++ docs/3/ColorSchemer.js
@@ -1,7 +1,7 @@
 class ColorSchemer {
-    static scheme(backColor='#FFFFFF', id='style-auto-color') {
+    static scheme(backColor='#FFFFFF', id='style-auto-color', query='.auto-color') {
         const colors = Color.getColors(backColor);
-        CSS.replace(colors, id);
+        CSS.replace(colors, id, query);
         return colors;
     }
 }
--- docs/3/main.js
+++ docs/3/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', (event) => {
     document.querySelector(`#back-color`).addEventListener('change', (event) => {
-        const colors = ColorSchemer.scheme(event.target.value);
+        const colors = ColorSchemer.scheme(event.target.value, id='id-style-auto-color-scheme', query='.auto-color-scheme');
         document.querySelector(`#fore-color`).setAttribute('value', colors.fore);
         document.querySelector(`#selected-back-color`).setAttribute('value', colors.selectedBack);
         document.querySelector(`#selected-fore-color`).setAttribute('value', colors.selectedFore);
